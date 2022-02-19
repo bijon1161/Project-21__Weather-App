245f734dd8ca2cc80a7ac4db2c0e97f1e480d9da
Bijon Mallik <bijon.ndc.pust@gmail.com>
2022-02-20 01:26:29 +0600

    Misspelling Correction

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -9,7 +9,7 @@ const forecast = (latitude, longitude, callback) => {
         } else if (body.error) {
             callback('Unable to find location', undefined)
         } else {
-            callback(undefined, body.current.weather_descriptions[0] + ". It is currently " + body.current.temperature + " degress out.")
+            callback(undefined, body.current.weather_descriptions[0] + ". It is currently " + body.current.temperature + " degrees out."+"There is a " + body.current.precip + "% chance of rain & the humidity is "+body.current.humidity)
         }
     })
 }
